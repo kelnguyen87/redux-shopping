46e6fce022c9596325d528f1bde3d1036e03f5b8
Luan Nguyen Thanh <luannt4@smartosc.com>
2020-07-27 11:20:05 +0700

    Updates

---
+++ precache-manifest.125db5776ddd68c06340652853be6fe0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6d51f8b52b9da010d68f70ab0f41ec",
+    "url": "/redux-shopping/index.html"
+  },
+  {
+    "revision": "988a2a472a8553cc931c",
+    "url": "/redux-shopping/static/css/2.53ff65a4.chunk.css"
+  },
+  {
+    "revision": "83916944d9edbd496a28",
+    "url": "/redux-shopping/static/css/main.08b60e54.chunk.css"
+  },
+  {
+    "revision": "988a2a472a8553cc931c",
+    "url": "/redux-shopping/static/js/2.8f3bfc1a.chunk.js"
+  },
+  {
+    "revision": "529fe0c0443a73d4c827a629df73a050",
+    "url": "/redux-shopping/static/js/2.8f3bfc1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83916944d9edbd496a28",
+    "url": "/redux-shopping/static/js/main.37cc300a.chunk.js"
+  },
+  {
+    "revision": "9c2cd72e5188313373b2",
+    "url": "/redux-shopping/static/js/runtime-main.788772b3.js"
+  }
+]);
